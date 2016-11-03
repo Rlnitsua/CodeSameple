23ce2935053a5ee528b8de886d1ea7b7545d31e2
BigUncleLi <BigUncleLi@163.com>
2016-11-03 18:25:12 +0800

    feat : add bubble sort
    for 1-n
      for 1-n
        if x[j] > x[j+1] swap(x, j, j+1)

---
+++ Section-1/src/org/uncle/lee/easy/BubbleSort.java
@@ -0,0 +1,33 @@
+package org.uncle.lee.easy;
+
+public class BubbleSort {
+	public int[] sort(int[] datas) {
+		int length = datas.length;
+		for (int i = 0; i < length - 1; i++) {
+			for (int j = 0; j < length - 1; j++) {
+				if (datas[j] > datas[j + 1]) {
+					swap(datas, j, j + 1);
+				}
+			}
+		}
+		return datas;
+	}
+
+	private void swap(int[] datas, int j, int i) {
+		int temp = datas[j];
+		datas[j] = datas[j + 1];
+		datas[j + 1] = temp;
+	}
+
+	public static void main(String[] args) {
+		int[] datas = { 3, 2, 8, 1, 9, 5, 4, 7, 6 };
+		int[] sortedDatas = new BubbleSort().sort(datas);
+		showDatas(sortedDatas);
+	}
+
+	private static void showDatas(int[] datas) {
+		for (Integer item : datas) {
+			System.out.print(item + " ");
+		}
+	}
+}
