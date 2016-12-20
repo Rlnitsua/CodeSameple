6f3024116e463c6e87f84579449c2836015a117a
BigUncleLi <BigUncleLi@163.com>
2016-12-20 10:47:57 +0800

    feat : add intersection of two array solution

---
+++ Section-1/src/org/uncle/lee/hash/table/IntersectionofTwoArrays_opt.java
@@ -0,0 +1,29 @@
+package org.uncle.lee.hash.table;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionofTwoArrays_opt {
+	public int[] intersection(int[] nums1, int[] nums2) {
+		Set<Integer> set1 = new HashSet<Integer>();
+		Set<Integer> intersectionSet = new HashSet<Integer>();
+		
+		for(int i = 0; i < nums1.length; i++){
+			set1.add(nums1[i]);
+		}
+		
+		for(int i = 0; i < nums2.length; i++){
+			if(set1.contains(nums2[i])){
+				intersectionSet.add(nums2[i]);
+			}
+		}
+		
+		int[] result = new int[intersectionSet.size()];
+		int index = 0;
+		for(Integer num : intersectionSet){
+			result[index++] = num;
+		}
+		
+		return result;
+	}
+}
