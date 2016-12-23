b6c35128a85b57c355b4ea97c0a23367ede6c133
BigUncleLi <BigUncleLi@163.com>
2016-12-23 14:56:23 +0800

    feat : add number of segment solution
    1. delete more blank
    2. delete /n /t

---
+++ Section-1/src/org/uncle/lee/string/NumberofSegmentsinaString.java
@@ -0,0 +1,45 @@
+package org.uncle.lee.string;
+
+import org.uncle.lee.log.LogUtils;
+
+public class NumberofSegmentsinaString {
+	private static final String TAG = "NumberofSegmentsinaString";
+	
+	public int countSegments(String s) {
+		if(s == null || s.trim().length() == 0){
+			return 0;
+		}
+
+		s = s.trim();
+		
+		char[] chArray = s.toCharArray();
+		int count = 0;
+		
+		for(int i = 0; i < chArray.length; i++){
+			if(chArray[i] == ' '){
+				count++;
+				while(i < chArray.length){
+					if(chArray[i + 1] == ' '){
+						i++;
+					}else {
+						break;
+					}
+				}
+			}else if (chArray[i] == '\n' || chArray[i] == '\t'){
+				count++;
+			}
+		}
+		
+		return ++count;
+	}
+	
+	public static void main(String[] args) {
+		// \b  \t  \n  \f  \r  \"  \'  \\
+//		String s = ", , , ,        a,    e\bae\fa\nddd\tsss\\wwwww";
+//		String s = " a         a    abc ";
+		String s = ", , , ,        a, eaefa";
+		LogUtils.d(TAG, "start");
+		LogUtils.d(TAG, s);
+		LogUtils.d(TAG, new NumberofSegmentsinaString().countSegments(s));
+	}
+}
