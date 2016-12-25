0a0dadd5ddd9829ab89a37dc9b70a0d344cbce79
BigUncleLi <BigUncleLi@163.com>
2016-12-25 18:20:02 +0800

    refactor : delete usless tag

--- Section-1/src/org/uncle/lee/hash/table/LoggerRateLimiter.java
+++ Section-1/src/org/uncle/lee/hash/table/LoggerRateLimiter.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LoggerRateLimiter {
-	private static final String TAG = "LoggerRateLimiter";
+//	private static final String TAG = "LoggerRateLimiter";
 	
 	public static void main(String[] args) {
 		
