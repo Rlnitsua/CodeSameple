93b027ade56fccc15cee32911b2d4f05b37b1e74
BigUncleLi <BigUncleLi@163.com>
2016-12-28 20:01:55 +0800

    feat : add closest bst opt solution

--- Section-1/src/org/uncle/lee/binary/tree/ClosestBinarySearchTreeValue.java
+++ Section-1/src/org/uncle/lee/binary/tree/ClosestBinarySearchTreeValue.java
@@ -1,36 +1,28 @@
 package org.uncle.lee.binary.tree;
 
+import org.uncle.lee.log.LogUtils;
+
 public class ClosestBinarySearchTreeValue {
-	private int error;
+	private static final String TAG = "ClosestBinarySearchTreeValue";
+
 	public int closestValue(TreeNode root, double target) {
-		if(root.left == null && root.right == null){
-			return root.val;
-		}else if(root.left == null){
-			if(Math.abs(root.right.val - target) < Math.abs(root.val - target)){
-				return closestValue(root.right, target);
-			}else {
-				return root.val;
-			}
-		}else if(root.right == null){
-			if(Math.abs(root.left.val - target) < Math.abs(root.val - target)){
-				return closestValue(root.left, target);
-			}else {
-				return root.val;
-			}
-		}
-		
-		if(Math.abs(root.right.val - target) < Math.abs(root.val - target)){
+		if (root.left != null && target <= (root.val + root.left.val) / 2) {
+			return closestValue(root.left, target);
+		} else if (root.right != null
+				&& target >= (root.val + root.right.val) / 2) {
 			return closestValue(root.right, target);
-		}else {
-			if(root.val <= target){
-				return root.val;
-			}else {
-				if(Math.abs(root.left.val - target) < Math.abs(root.val - target)){
-					return closestValue(root.left, target);
-				}else {
-					return root.val;
-				}
-			}
+		} else {
+			return root.val;
 		}
+	}
+
+	public static void main(String[] args) {
+		TreeNode node1 = new TreeNode(8);
+		TreeNode node2 = new TreeNode(1);
+		node1.left = node2;
+
+		LogUtils.d(TAG, "start");
+		LogUtils.d(TAG,
+				new ClosestBinarySearchTreeValue().closestValue(node1, 6));
 	}
 }
