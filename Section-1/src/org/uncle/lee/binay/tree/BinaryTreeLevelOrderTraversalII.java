24153232970e7998213580d66b484a6def90d6f5
BigUncleLi <BigUncleLi@163.com>
2016-12-29 20:19:58 +0800

    feat : add bt level order solution

---
+++ Section-1/src/org/uncle/lee/binay/tree/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,46 @@
+package org.uncle.lee.binay.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uncle.lee.log.LogUtils;
+
+public class BinaryTreeLevelOrderTraversalII {
+	private static final String TAG = "BinaryTreeLevelOrderTraversalII";
+	
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		if(root == null){
+			return null;
+		}
+		List<List<Integer>> valList = new ArrayList<List<Integer>>();
+		addValList(valList, root, 0);
+		return valList;
+	}
+
+	private void addValList(List<List<Integer>> valList, TreeNode root, int deep) {
+		if(root == null){
+			return;
+		}
+		valList.add(new ArrayList<Integer>());
+		
+		addValList(valList, root.left, deep + 1);
+		addValList(valList, root.right, deep + 1);
+		valList.get(deep).add(root.val);
+	}
+	
+	public static void main(String[] args) {
+		TreeNode node1 = new TreeNode(3);
+		TreeNode node2 = new TreeNode(9);
+		TreeNode node3 = new TreeNode(20);
+		TreeNode node4 = new TreeNode(15);
+		TreeNode node5 = new TreeNode(7);
+		
+		node1.left = node2;
+		node1.right = node3;
+		node3.left = node4;
+		node3.right = node5;
+		
+		LogUtils.d(TAG, "start");
+		LogUtils.d(TAG, new BinaryTreeLevelOrderTraversalII().levelOrderBottom(node1));
+	}
+}
