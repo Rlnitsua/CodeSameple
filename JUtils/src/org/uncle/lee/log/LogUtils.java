b5e2b11e9789a0c9e7af0c9933d816c72aa3c5fd
BigUncleLi <BigUncleLi@163.com>
2017-01-05 11:52:45 +0800

    chore : add ListNode operator in logutil

--- JUtils/src/org/uncle/lee/log/LogUtils.java
+++ JUtils/src/org/uncle/lee/log/LogUtils.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.uncle.lee.time.TimeUtils;
+import org.uncle.lee.util.ListNode;
 
 public  class  LogUtils{
 	public static void d(String TAG, Object xMsg){
@@ -39,6 +40,35 @@ public  class  LogUtils{
 		System.out.println(content);
 	}
 	
+	private static String getTwoDimensionArrayMsg(int[][] array) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");
+		for(int i = 0; i < array.length; i++){
+			sb.append(Arrays.toString(array[i]));
+		}
+		sb.append("]");
+		return new String(sb);
+	}
+	
+	public static void d(String TAG, ListNode node){
+		String logLevel = "D/";
+		String msg = getListNodeValues(node);
+		String content = constructLogContent(logLevel, TAG, msg);
+		System.out.println(content);
+	}
+	
+	private static String getListNodeValues(ListNode node) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");
+		while(node != null){
+			sb.append(node.val + "->");
+			node = node.next;
+		}
+		sb.delete(sb.length() - 3, sb.length() - 1);
+		sb.append("]");
+		return new String(sb);
+	}
+
 	private static String constructLogContent(String logLevel, String TAG, String msg) {
 		StringBuffer sb = new StringBuffer();
 		sb.append(getCurrentTime());
@@ -54,15 +84,5 @@ public  class  LogUtils{
 	
 	private static String getTagMessage(String TAG) {
 		return TAG + ": ";
-	}
-	
-	private static String getTwoDimensionArrayMsg(int[][] array) {
-		StringBuffer sb = new StringBuffer();
-		sb.append("[");
-		for(int i = 0; i < array.length; i++){
-			sb.append(Arrays.toString(array[i]));
-		}
-		sb.append("]");
-		return new String(sb);
 	}
 }
