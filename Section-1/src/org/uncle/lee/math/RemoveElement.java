3983dd840b7fc40faf561ccb4b7077299d5a9c35
BigUncleLi <BigUncleLi@163.com>
2017-01-06 18:19:43 +0800

    feat : add remove element solution

---
+++ Section-1/src/org/uncle/lee/math/RemoveElement.java
@@ -0,0 +1,24 @@
+package org.uncle.lee.math;
+
+import org.uncle.lee.log.LogUtils;
+
+public class RemoveElement {
+	private static final String TAG = "RemoveElement";
+	
+	public int removeElement(int[] nums, int val) {
+		int realIndex = 0;
+		for(int index = 0; index < nums.length; index++){
+			if(nums[index] != val){
+				nums[realIndex] = nums[index];
+				realIndex++;
+			}
+		}
+		return realIndex;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {3, 2, 2, 3};
+		LogUtils.d(TAG, "start");
+		LogUtils.d(TAG, new RemoveElement().removeElement(nums, 3));
+	}
+}
