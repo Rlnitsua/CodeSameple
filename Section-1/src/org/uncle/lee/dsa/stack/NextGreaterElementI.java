2282c48ae19b716bcdf3d2851474724dc0819fab
BigUncleLi <BigUncleLi@163.com>
2017-02-08 16:26:36 +0800

    feat : add next greater element solution

---
+++ Section-1/src/org/uncle/lee/dsa/stack/NextGreaterElementI.java
@@ -0,0 +1,42 @@
+package org.uncle.lee.dsa.stack;
+
+import org.uncle.lee.log.LogUtils;
+
+public class NextGreaterElementI {
+	public static final String TAG = "NextGreaterElementI";
+	
+	public int[] nextGreaterElement(int[] findNums, int[] nums) {
+		if(findNums == null){
+			return null;
+		}
+		
+		for(int i = 0; i < findNums.length; i++){
+			int index = -1;
+			for(int j = 0; j < nums.length; j++){
+				if(findNums[i] == nums[j]){
+					index = j;
+					break;
+				}
+			}
+			
+			boolean findResult = false;
+			for(int j = (index + 1); j < nums.length; j++){
+				if(findNums[i] < nums[j]){
+					findResult = true;
+					findNums[i] = nums[j];
+					break;
+				}
+			}
+			
+			if(!findResult){
+				findNums[i] = -1;
+			}
+		}
+		return findNums;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums1 = {4,1,2}, nums2 = {1,3,4,2};
+		LogUtils.d(TAG, new NextGreaterElementI().nextGreaterElement(nums1, nums2));
+	}
+}
