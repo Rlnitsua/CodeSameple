0328be9bda8b690fb7cb8b33917b42df1175dfe0
BigUncleLi <BigUncleLi@163.com>
2017-02-10 11:09:48 +0800

    feat : add best time buy and sell stock solution

---
+++ Section-1/src/org/uncle/lee/greedy/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,23 @@
+package org.uncle.lee.greedy;
+
+import org.uncle.lee.log.LogUtils;
+
+public class BestTimetoBuyandSellStockII {
+	private static final String TAG = "BestTimetoBuyandSellStockII";
+	
+	public int maxProfit(int[] prices) {
+		int profit = 0;
+		
+		for(int i = 0; i < prices.length - 1; i++){
+			if(prices[i + 1] > prices[i]){
+				profit += prices[i + 1] - prices[i];
+			}
+		}
+		return profit;
+	}
+	
+	public static void main(String[] args) {
+		int[] prices = {10, 0, 11, 3, 5};
+		LogUtils.d(TAG, new BestTimetoBuyandSellStockII().maxProfit(prices));
+	}
+}
