a40ecef654a4d3715c5b0cb03d49b9535b0bd278
BigUncleLi <BigUncleLi@163.com>
2017-02-15 13:08:25 +0800

    feat : add path sum solution

---
+++ Section-1/src/org/uncle/lee/binary/tree/PathSum.java
@@ -0,0 +1,43 @@
+package org.uncle.lee.binary.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uncle.lee.util.TreeNode;
+
+public class PathSum {
+	List<Integer> list = new ArrayList<Integer>();
+	public boolean hasPathSum0(TreeNode root, int sum) {
+		if(root == null){
+			return false;
+		}
+		
+		calculateSum(root, 0);
+		return list.contains(sum);
+	}
+	
+	public boolean hasPathSum(TreeNode root, int sum){
+		if(root == null){
+			return false;
+		}
+		if(root.left == null && root.right == null && sum - root.val == 0){
+			return true;
+		}
+		return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+	}
+	
+	private void calculateSum(TreeNode root, int sum) {
+		sum += root.val;
+		
+		if(root.left == null && root.right == null){
+			list.add(sum);
+		} else if (root.left != null && root.right == null){
+			calculateSum(root.left, sum);
+		} else if (root.left == null && root.right != null){
+			calculateSum(root.right, sum);
+		} else {
+			calculateSum(root.left, sum);
+			calculateSum(root.right, sum);
+		}
+	}
+}
