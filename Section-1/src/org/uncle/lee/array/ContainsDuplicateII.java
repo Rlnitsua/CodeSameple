d7d7cfee6cda19ab9d3389a93668e452b4d5be4e
BigUncleLi <BigUncleLi@163.com>
2017-02-17 15:08:00 +0800

    feat : add contains duplicate solution

---
+++ Section-1/src/org/uncle/lee/array/ContainsDuplicateII.java
@@ -0,0 +1,47 @@
+package org.uncle.lee.array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.uncle.lee.log.LogUtils;
+
+public class ContainsDuplicateII {
+	private static final String TAG = "ContainsDuplicateII";
+	
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+		if(nums == null || nums.length == 0){
+			return false;
+		}
+		
+		Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+		
+		for(int i = 0; i < nums.length; i++){
+			if(!map.containsKey(nums[i])){
+				List<Integer> list = new ArrayList<Integer>();
+				list.add(i);
+				map.put(nums[i], list);
+			} else {
+				map.get(nums[i]).add(i);
+			}
+		}
+
+		for(Entry<Integer, List<Integer>> entry : map.entrySet()){
+			for(int i = 0; i < entry.getValue().size() - 1; i++){
+				if(entry.getValue().get(i + 1) - entry.getValue().get(i) <= k){
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {1, 2, 3, 4, 2, 7};
+		LogUtils.d(TAG, "start");
+		LogUtils.d(TAG, new ContainsDuplicateII().containsNearbyDuplicate(nums, 3));
+	}
+}
