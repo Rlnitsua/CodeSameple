1888731bfd3951bcf2feef7025940394f0cdc3fc
BigUncleLi <BigUncleLi@163.com>
2017-02-21 11:50:28 +0800

    feat : add detect captial solution

---
+++ Section-1/src/org/uncle/lee/string/DetectCapital.java
@@ -0,0 +1,40 @@
+package org.uncle.lee.string;
+
+public class DetectCapital {
+	public boolean detectCapitalUse(String word) {
+		if(word.length() == 1) {
+			return true;
+		} else if (word.length() == 2) {
+			if(isLowercase(word.charAt(0)) && !isLowercase(word.charAt(1))) {
+				return false;
+			}
+		}
+		
+		if(!isLowercase(word.charAt(0)) && !isLowercase(word.charAt(1))) {
+			for(int i = 2; i < word.length(); i++) {
+				if(isLowercase(word.charAt(i))) {
+					return false;
+				}
+			}
+		} else if (!isLowercase(word.charAt(0)) && isLowercase(word.charAt(1))) {
+			for(int i = 2; i < word.length(); i++) {
+				if(!isLowercase(word.charAt(i))) {
+					return false;
+				}
+			}
+		} else if (isLowercase(word.charAt(0))) {
+			for(int i = 1; i < word.length(); i++) {
+				if(!isLowercase(word.charAt(i))) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	private boolean isLowercase(char ch) {
+		return ch >= 'a' && ch <= 'z';
+	}
+
+}
