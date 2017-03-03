5830c8dce1cbfc5dc2e2ea6bdf72d939bcaa6e1c
BigUncleLi <BigUncleLi@163.com>
2017-03-03 18:06:20 +0800

    feat : add rotate array solution

---
+++ Section-1/src/org/uncle/lee/array/RotateArray.java
@@ -0,0 +1,25 @@
+package org.uncle.lee.array;
+
+import java.util.Arrays;
+
+import org.uncle.lee.log.LogUtils;
+
+public class RotateArray {
+	private static final String TAG = "RotateArray";
+	
+	public void rotate(int[] nums, int k) {
+		k %= nums.length;
+		
+		int[] temp = Arrays.copyOf(nums, nums.length);
+
+		System.arraycopy(temp, temp.length - k, nums, 0, k);
+		System.arraycopy(temp, 0, nums, k, nums.length - k);
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {1, 2, 3, 4, 5, 6, 7};
+//		int[] nums = {1};
+		new RotateArray().rotate(nums, 3);
+		LogUtils.d(TAG, nums);
+	}
+}
