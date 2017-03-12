3827f9507b2509f5e99b981a2a3db8d942042abf
BigUncleLi <BigUncleLi@163.com>
2017-03-12 15:36:42 +0800

    feat : add find all duplicates solution

---
+++ Section-1/src/org/uncle/lee/array/FindAllDuplicatesinanArray.java
@@ -0,0 +1,29 @@
+package org.uncle.lee.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uncle.lee.log.LogUtils;
+
+public class FindAllDuplicatesinanArray {
+	private static final String TAG = "FindAllDuplicatesinanArray";
+	
+	public List<Integer> findDuplicates(int[] nums) {
+		List<Integer> res = new ArrayList<>();
+		for(int num : nums){
+			int index = Math.abs(num) - 1;
+			
+			if(nums[index] < 0){
+				res.add(Math.abs(num));
+			} else {
+				nums[index] = -nums[index];
+			}
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {4,3,2,7,8,2,3,1};
+		LogUtils.d(TAG, new FindAllDuplicatesinanArray().findDuplicates(nums));
+	}
+}
