43110f4741247a8c3f9440354147f76e7caff4fe
BigUncleLi <BigUncleLi@163.com>
2017-03-21 18:06:50 +0800

    feat : add beautiful arrangement solution

--- Section-1/src/org/uncle/lee/back/tracking/BeautifulArrangement.java
+++ Section-1/src/org/uncle/lee/back/tracking/BeautifulArrangement.java
@@ -1,29 +1,35 @@
 package org.uncle.lee.back.tracking;
 
-@Deprecated
+import org.uncle.lee.log.LogUtils;
+
 public class BeautifulArrangement {
+	private static final String TAG = "BeautifulArrangement";
+	
+	private int count = 0;
+	
 	public int countArrangement(int N) {
-		if(N == 2){
-			return 2;
+		doHandle(N, 1, new int[N + 1]);
+		return count;
+	}
+	
+	private void doHandle(int n, int position, int[] used) {
+		if(position > n){
+			count++;
+			return;
 		}
 		
-		int count = 0;
-		int[] arr = new int[N];
-		
-		for(int i = 1; i <= N; i++){
-			arr[i - 1] = i;
-		}
-		
-		for(int i = 1; i <= N; i++){
-			for(int j = 2; j <= N; j++){
-				if(i * j <= arr[N - 1]){
-					count += 2;
-				} else {
-					break;
-				}
+		for(int i = 1; i <= n; i++){
+			if(used[i] == 0 && (i % position == 0 || position % i == 0)){
+				used[i] = 1;
+				doHandle(n, position + 1, used);
+				used[i] = 0;
 			}
 		}
-		
-		return count;
+	}
+
+	public static void main(String[] args) {
+		LogUtils.d(TAG, new BeautifulArrangement().countArrangement(4));
+		LogUtils.d(TAG, new BeautifulArrangement().countArrangement(5));
+		LogUtils.d(TAG, new BeautifulArrangement().countArrangement(6));
 	}
 }
