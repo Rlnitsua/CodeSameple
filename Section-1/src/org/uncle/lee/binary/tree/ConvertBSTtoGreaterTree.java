7fc5b0db34442d1eb9ad990724249b9c73bf7f37
BigUncleLi <BigUncleLi@163.com>
2017-03-27 14:41:47 +0800

    feat : add convert BST T greater Tree solution

---
+++ Section-1/src/org/uncle/lee/binary/tree/ConvertBSTtoGreaterTree.java
@@ -0,0 +1,62 @@
+package org.uncle.lee.binary.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uncle.lee.log.LogUtils;
+import org.uncle.lee.util.TreeNode;
+
+public class ConvertBSTtoGreaterTree {
+	private static final String TAG = "ConvertBSTtoGreaterTree";
+	
+	public TreeNode convertBST(TreeNode root) {
+		List<Integer> numList = new ArrayList<>();
+		getInorderList(root, numList);
+		handleRoot(root, numList);
+		return root;
+	}
+	
+	private void getInorderList(TreeNode root, List<Integer> numList) {
+		if(root == null){
+			return;
+		}
+		getInorderList(root.left, numList);
+		numList.add(root.val);
+		getInorderList(root.right, numList);
+	}
+	
+	private void handleRoot(TreeNode root, List<Integer> numList) {
+		if(root == null){
+			return;
+		}
+		handleRoot(root.left, numList);
+		//handle action
+		root.val = getVal(numList, root.val);
+		handleRoot(root.right, numList);
+	}
+
+	private int getVal(List<Integer> numList, int val) {
+		int index = numList.indexOf(val);
+		int sum = 0;
+		for(int i = index; i < numList.size(); i++){
+			sum += numList.get(i);
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		LogUtils.d(TAG, "start");
+		TreeNode node1 = new TreeNode(2);
+		TreeNode node2 = new TreeNode(0);
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node4 = new TreeNode(-4);
+		TreeNode node5 = new TreeNode(1);
+		node1.left = node2;
+		node1.right = node3;
+		node2.left = node4;
+		node2.right = node5;
+		
+		ConvertBSTtoGreaterTree convertBSTtoGreaterTree = new ConvertBSTtoGreaterTree();
+		LogUtils.d(TAG, convertBSTtoGreaterTree.convertBST(node1));
+	}
+}
