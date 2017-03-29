de2b94e03ea11807bb560c7fcf2fb3f4fd198ff7
BigUncleLi <BigUncleLi@163.com>
2017-03-29 19:49:14 +0800

    feat : add ternary expression parese solution

---
+++ Section-1/src/org/uncle/lee/deep/search/TernaryExpressionParser.java
@@ -0,0 +1,9 @@
+package org.uncle.lee.deep.search;
+
+public class TernaryExpressionParser {
+	public String parseTernary(String expression) {
+		return null;
+	}
+	
+	private void parseTernary(Char booleanFlag, String expression, int ){}
+}
