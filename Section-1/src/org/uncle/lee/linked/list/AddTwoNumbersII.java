01c4911eecfb4b147005789ea03d7ce791376c15
BigUncleLi <BigUncleLi@163.com>
2017-04-01 19:05:41 +0800

    feat : add add two number II solution

---
+++ Section-1/src/org/uncle/lee/linked/list/AddTwoNumbersII.java
@@ -0,0 +1,54 @@
+package org.uncle.lee.linked.list;
+
+import java.util.Stack;
+import org.uncle.lee.log.LogUtils;
+import org.uncle.lee.util.ListNode;
+import org.uncle.lee.util.ListNodeCreater;
+
+public class AddTwoNumbersII {
+	private static final String TAG = "AddTwoNumbersII";
+	
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		Stack<Integer> stack1 = getQueue(l1);
+		Stack<Integer> stack2 = getQueue(l2);
+		return addTwoStack(stack1, stack2);
+	}
+	
+	private Stack<Integer> getQueue(ListNode node) {
+		Stack<Integer> stack = new Stack<Integer>();
+		while(node != null){
+			stack.push(node.val);
+			node = node.next;
+		}
+		return stack;
+	}
+
+	private ListNode addTwoStack(Stack<Integer> stack1, Stack<Integer> stack2) {
+		ListNode head = null;
+		boolean isOverride = false;
+		while(stack1.size() != 0 || stack2.size() != 0){
+			int part1 = (stack1.size() == 0) ? 0 : stack1.pop();
+			int part2 = (stack2.size() == 0) ? 0 : stack2.pop();
+			ListNode node = new ListNode((part1 + part2 + (isOverride ? 1 : 0))%10);
+			isOverride = ((part1 + part2 + (isOverride ? 1 : 0)) >= 10) ? true : false;
+			node.next = head;
+			head = node;
+		}
+		
+		if(isOverride){
+			ListNode node = new ListNode(1);
+			node.next = head;
+			head = node;
+		}
+		
+		return head;
+	}
+
+	public static void main(String[] args) {
+		ListNode l1 = ListNodeCreater.create("7,2,4,3");
+		ListNode l2 = ListNodeCreater.create("5,6,4");
+		
+		AddTwoNumbersII addTwoNumbersII = new AddTwoNumbersII();
+		LogUtils.d(TAG, addTwoNumbersII.addTwoNumbers(l1, l2));
+	}
+}
