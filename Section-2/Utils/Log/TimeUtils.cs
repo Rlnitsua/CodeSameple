9eda84ac501cdacf007600c5b9f941d507422ac7
BigUncleLi <BigUncleLi@163.com>
2017-04-09 19:18:49 +0800

    feat : add time utils

---
+++ Section-2/Utils/Log/TimeUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Log
+{
+    public class TimeUtils
+    {
+        public static String GetCurrentTime()
+        {
+            String timePattern = "MM-MM HH:mm:ss.fffff";
+            return new DateTime().ToString(timePattern);
+        }
+    }
+}
