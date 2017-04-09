b774e2180c2557970823daa0579dc9da96636c84
BigUncleLi <BigUncleLi@163.com>
2017-04-09 19:18:35 +0800

    feat : add logutils

---
+++ Section-2/Utils/Log/LogUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Log
+{
+    public class LogUtils
+    {
+        public static String dVersion = "D";
+        public static String colonTag = ":";
+        public static String slashTag = "/";
+        public static String blankTag = " ";
+
+        public static void D(String TAG, String content)
+        {
+            String currentTime = TimeUtils.GetCurrentTime();
+            String fileTag = dVersion + slashTag + TAG + colonTag + blankTag;
+            Console.WriteLine(currentTime + blankTag + fileTag + blankTag + content);
+        }
+    }
+}
