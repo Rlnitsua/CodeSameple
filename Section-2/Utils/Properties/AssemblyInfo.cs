40dc9ec992d31e0b3da4fba0f439738a3b144f0a
BigUncleLi <BigUncleLi@163.com>
2017-04-09 19:19:07 +0800

    feat : add utils project

--- Section-2/Utils/Properties/AssemblyInfo.cs
+++ Section-2/Utils/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("c4be5a7c-0aa1-4d8e-9cd2-db3cf15494d9")]
+[assembly: Guid("316d5e5b-ec62-4064-b6bd-79b48ee2944a")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      修订号
 //
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
-// 方法是按如下所示使用“*”: :
+//通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
