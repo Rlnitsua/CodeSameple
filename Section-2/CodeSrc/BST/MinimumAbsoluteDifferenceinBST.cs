1afbd16435f705e7d0b9424e0e4e4d0de566dc67
BigUncleLi <BigUncleLi@163.com>
2017-04-13 19:01:23 +0800

    feat : add mini diff in bst solution

---
+++ Section-2/CodeSrc/BST/MinimumAbsoluteDifferenceinBST.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.DS;
+
+namespace CodeSrc.BST
+{
+    class MinimumAbsoluteDifferenceinBST
+    {
+        public int GetMinimumDifference(TreeNode root)
+        {
+            List<int> valList = new List<int>();
+            GetList(root, valList);
+
+            return GetMinDiff(valList);
+        }
+
+        private void GetList(TreeNode root, List<int> valList)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            GetList(root.left, valList);
+            valList.Add(root.val);
+            GetList(root.right, valList);
+        }
+
+        private int GetMinDiff(List<int> valList)
+        {
+            int minDiff = int.MaxValue;
+            for (int i = 0; i < (valList.Count - 1); i++)
+            {
+                minDiff = Math.Min(minDiff, Math.Abs(valList[i + 1] - valList[i]));
+            }
+            return minDiff;
+        }
+    }
+}
