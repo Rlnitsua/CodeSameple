39a89283e3137bdcd85ac53b8389c05ed03e429e
BigUncleLi <BigUncleLi@163.com>
2017-04-24 11:05:43 +0800

    feat : update longest usb solution

--- Section-2/CodeSrc/StringType/LongestUncommonSubsequenceI.cs
+++ Section-2/CodeSrc/StringType/LongestUncommonSubsequenceI.cs
@@ -1,78 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Utils.Log;
 
 namespace CodeSrc.StringType
 {
     class LongestUncommonSubsequenceI
     {
-        private const String tag = "LongestUncommonSubsequenceI";
+        private const string Tag = "LongestUncommonSubsequenceI";
 
         public int FindLUSlength(string a, string b)
         {
-            Dictionary<char, int> dic = new Dictionary<char, int>();
-            AddChIntoDic(dic, a.ToCharArray());
-            AddWithDelete(dic, b.ToCharArray());
-            int commomElementCount = a.Length + b.Length - CountDic(dic);
-            return Math.Min(a.Length, b.Length) - commomElementCount;
-        }
-
-        private void AddChIntoDic(Dictionary<char, int> dic, char[] chArr)
-        {
-            foreach (char ch in chArr)
+            if (a.Length != b.Length)
             {
-                if (dic.ContainsKey(ch))
-                {
-                    dic[ch]++;
-                }
-                else
-                {
-                    dic.Add(ch, 1);
-                }
+                return Math.Max(a.Length, b.Length);
+            }
+            else
+            {
+                return a.Equals(b) ? -1 : a.Length;
             }
         }
-
-        private void AddWithDelete(Dictionary<char, int> dic, char[] chArr)
+        
+        public static void Main(string[] args)
         {
-            foreach (char ch in chArr)
-            {
-                if (dic.ContainsKey(ch))
-                {
-                    if (dic[ch] == 1)
-                    {
-                        dic.Remove(ch);
-                    }
-                    else
-                    {
-                        dic[ch]--;
-                    }
-                }
-                else
-                {
-                    dic.Add(ch, 1);
-                }
-            }
-        }
-
-        private int CountDic(Dictionary<char, int> dic)
-        {
-            int count = 0;
-            foreach (char ch in dic.Keys)
-            {
-                count += dic[ch];
-            }
-            return count;
-        }
-
-        public static void Main(String[] args)
-        {
-            String a = "aba";
-            String b = "cdc";
-            LongestUncommonSubsequenceI unCommomS = new LongestUncommonSubsequenceI();
-            LogUtils.D(tag, unCommomS.FindLUSlength(a, b));
+            const string a = "aba";
+            const string b = "cdc";
+            var unCommomS = new LongestUncommonSubsequenceI();
+            LogUtils.D(Tag, unCommomS.FindLUSlength(a, b));
         }
     }
 }
