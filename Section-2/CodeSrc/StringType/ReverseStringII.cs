c7720d83978fff88a8929b38b386b5b4066a9061
BigUncleLi <BigUncleLi@163.com>
2017-04-24 11:06:06 +0800

    feat : update reverse stringII solutio

--- Section-2/CodeSrc/StringType/ReverseStringII.cs
+++ Section-2/CodeSrc/StringType/ReverseStringII.cs
@@ -3,45 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utils.Log;
 
 namespace CodeSrc.StringType
 {
     class ReverseStringII
     {
+        private const string TAG = "ReverseStringII";
+
         public string ReverseStr(string s, int k)
         {
-            if (k >= s.Length)
+            var res = new StringBuilder();
+            for (int i = 0; i < s.Length; i += 2 * k)
             {
-                return ReverseKIndexStr(s, s.Length);
+                int lastIndex = (i + 2 * k - 1) >= s.Length ? s.Length - i : 2 * k;
+                res.Append(HandleTarget(s.Substring(i, lastIndex), k));
+            }
+            return res.ToString();
+        }
+
+        private string HandleTarget(string target, int k)
+        {
+            if (target.Length < k)
+            {
+                return ReverseTarget(target);
             }
             else
             {
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < (s.Length / k); i += 2)
-                {
-                    sb.Append(ReverseKIndexStr(s.Substring(i * k, 2 * k), k));
-                }
-                return sb.ToString();
+                string res = ReverseTarget(target.Substring(0, k));
+                res += target.Substring(k, target.Length - k);
+                return res;
             }
         }
 
-        private string ReverseKIndexStr(string s, int k)
+        private string ReverseTarget(string substring)
         {
-            StringBuilder sb = new StringBuilder();
             Stack<char> stack = new Stack<char>();
-            for (int i = 0; i < k; i++)
+            foreach (char ch in substring.ToCharArray())
             {
-                stack.Push(s[i]);
+                stack.Push(ch);
             }
+            StringBuilder sb = new StringBuilder();
             while (stack.Count != 0)
             {
                 sb.Append(stack.Pop());
             }
-            for (int i = k; i < s.Length; i++)
-            {
-                sb.Append(s[i]);
-            }
             return sb.ToString();
+        }
+
+        public static void Main(string[] args)
+        {
+            const string s = "abcdefg";
+            var demo = new ReverseStringII();
+            LogUtils.D(TAG, demo.ReverseStr(s, 2));
         }
     }
 }
