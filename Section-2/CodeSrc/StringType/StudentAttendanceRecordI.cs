efdf75112e87697a04407f7277b4bc42ae29f334
BigUncleLi <BigUncleLi@163.com>
2017-04-25 13:49:41 +0800

    feat : add student attend record solution

---
+++ Section-2/CodeSrc/StringType/StudentAttendanceRecordI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSrc.StringType
+{
+    class StudentAttendanceRecordI
+    {
+        public bool CheckRecord(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return false;
+            }
+
+            int aCount = 0;
+            int lCount = 0;
+
+            foreach (char ch in s.ToCharArray())
+            {
+                if (ch.Equals('A'))
+                {
+                    lCount = 0;
+                    aCount++;
+                }
+                else if (ch.Equals('L'))
+                {
+                    lCount++;
+                }
+                else
+                {
+                    lCount = 0;
+                }
+                if (aCount >= 2 || lCount >= 3)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
