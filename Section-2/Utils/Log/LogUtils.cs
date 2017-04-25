f37ea166968c1c8c8ec3d7a4f442b4f398360598
BigUncleLi <BigUncleLi@163.com>
2017-04-25 13:50:00 +0800

    feat : add new D override method

--- Section-2/Utils/Log/LogUtils.cs
+++ Section-2/Utils/Log/LogUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,23 +9,51 @@ namespace Utils.Log
 {
     public class LogUtils
     {
-        private static String dVersion = "D";
-        private static String colonTag = ":";
-        private static String slashTag = "/";
-        private static String blankTag = " ";
+        private static string dVersion = "D";
+        private static string colonTag = ":";
+        private static string slashTag = "/";
+        private static string blankTag = " ";
 
-        public static void D(String TAG, String content)
+        public static void D(string tag, String content)
         {
-            String currentTime = TimeUtils.GetCurrentTime();
-            String fileTag = dVersion + slashTag + TAG + colonTag;
+            string currentTime = TimeUtils.GetCurrentTime();
+            string fileTag = dVersion + slashTag + tag + colonTag;
             Console.WriteLine(currentTime + blankTag + fileTag + blankTag + content);
         }
 
-        public static void D(String TAG, int content)
+        public static void D(string tag, bool content)
         {
-            String currentTime = TimeUtils.GetCurrentTime();
-            String fileTag = dVersion + slashTag + TAG + colonTag + blankTag;
+            string currentTime = TimeUtils.GetCurrentTime();
+            string fileTag = dVersion + slashTag + tag + colonTag;
             Console.WriteLine(currentTime + blankTag + fileTag + blankTag + content);
+        }
+
+        public static void D(string tag, int content)
+        {
+            string currentTime = TimeUtils.GetCurrentTime();
+            string fileTag = dVersion + slashTag + tag + colonTag + blankTag;
+            Console.WriteLine(currentTime + blankTag + fileTag + blankTag + content);
+        }
+
+        public static void D<T>(string tag, IList<T> list)
+        {
+            string currentTime = TimeUtils.GetCurrentTime();
+            string fileTag = dVersion + slashTag + tag + colonTag + blankTag;
+            string content = GetContent<T>(list);
+            Console.WriteLine(currentTime + blankTag + fileTag + blankTag + content);
+        }
+
+        private static string GetContent<T>(IList<T> list)
+        {
+            StringBuilder content = new StringBuilder();
+            content.Append("[");
+            foreach (T e in list)
+            {
+                content.Append(e + ",");
+            }
+            content.Remove(content.Length - 1, 1);
+            content.Append("]");
+            return content.ToString();
         }
     }
 }
