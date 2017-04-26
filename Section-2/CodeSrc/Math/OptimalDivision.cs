8d78943c0123552b8cf385380f03a98bedd09746
BigUncleLi <BigUncleLi@163.com>
2017-04-26 18:26:38 +0800

    feat : add optimal division solution

---
+++ Section-2/CodeSrc/Math/OptimalDivision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSrc.Math
+{
+    class OptimalDivision
+    {
+        public string OptimalDivisionM(int[] nums)
+        {
+            return null;
+        }
+    }
+}
