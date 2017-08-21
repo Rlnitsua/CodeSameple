11f837c23fada60b2b22a9d7889cfd272fde3d4e
BigUncleLi <BigUncleLi@163.com>
2017-08-21 14:31:08 +0800

    feat : add Judge Route Circle solution

---
+++ Section-1/src/org/uncle/lee/string/JudgeRouteCircle.java
@@ -0,0 +1,46 @@
+package org.uncle.lee.string;
+
+import org.uncle.lee.log.LogUtils;
+
+public class JudgeRouteCircle {
+	private static final String TAG = "JudgeRouteCircle";
+	
+	public boolean judgeCircle(String moves) {
+		if(moves == null) {
+			return false;
+		}
+		
+		int upCount = 0;
+		int leftCount = 0;
+
+		char[] arr = moves.toCharArray();
+
+		for (char ch : arr) {
+			switch (ch) {
+				case 'U':
+					upCount++;
+					break;
+				case 'D':
+					upCount--;
+					break;
+				case 'L':
+					leftCount++;
+					break;
+				case 'R':
+					leftCount--;
+					break;
+			}
+		}
+		
+		if(upCount == 0 && leftCount == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		String msg = "DURDLDRRLL";
+		LogUtils.d(TAG, new JudgeRouteCircle().judgeCircle(msg));
+	}
+}
