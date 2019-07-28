4ddd32cee5e5caab1f03421b0090345961b1a7b1
BigUncleLi <BigUncleLi@163.com>
2019-07-28 14:31:43 +0800

    feat : add MinimumAbsoluteDifference solution

---
+++ Section-1/src/org/uncle/lee/contest/MinimumAbsoluteDifference.java
@@ -0,0 +1,46 @@
+package org.uncle.lee.contest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uncle.lee.log.LogUtils;
+
+public class MinimumAbsoluteDifference {
+	private static final String TAG = "MinimumAbsoluteDifference";
+	
+	public List<List<Integer>> minimumAbsDifference(int[] arr) {
+		// sort
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		// find minimum absolute diff
+		int minimum = Integer.MAX_VALUE;
+		for (int i = 0; i < arr.length - 1; i++) {
+			minimum = Math.min(minimum, arr[i + 1] - arr[i]);
+		}
+		// find res
+		List<List<Integer>> res = new ArrayList<>();
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i + 1] - arr[i] == minimum) {
+				List<Integer> item = new ArrayList<>();
+				item.add(arr[i]);
+				item.add(arr[i + 1]);
+				res.add(item);
+			}
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		MinimumAbsoluteDifference difference = new MinimumAbsoluteDifference();
+		LogUtils.d(TAG, difference.minimumAbsDifference(new int[] {4,2,1,3}));
+		LogUtils.d(TAG, difference.minimumAbsDifference(new int[] {1,3,6,10,15}));
+		LogUtils.d(TAG, difference.minimumAbsDifference(new int[] {3,8,-10,23,19,-4,-14,27}));
+	}
+}
