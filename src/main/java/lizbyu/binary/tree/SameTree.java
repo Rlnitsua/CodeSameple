4f3627df190ef8049d60a41e7b7ac03339293113
Uybzil <BigUncleLi@163.com>
2020-02-26 11:33:31 +0800

    feat : add same tree implement

--- src/main/java/lizbyu/binary/tree/SameTree.java
+++ src/main/java/lizbyu/binary/tree/SameTree.java
@@ -24,14 +24,32 @@ public class SameTree {
 				return false;
 			}
 		}
-		
+
 		if((p.left == null && q.left != null) || (p.left != null && q.left == null)){
 			return false;
 		}
 		if((p.right == null && q.right != null) || (p.right != null && q.right == null)){
 			return false;
 		}
-		
+
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+
+	public boolean isSameTree0(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		}
+
+		if (p == null || q == null) {
+			return false;
+		} else if (p.val != q.val) {
+			return false;
+		}
+
+		if (p.left == null && p.right == null && q.left == null && q.right == null) {
+			return true;
+		}
+
 		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 	}
 }
