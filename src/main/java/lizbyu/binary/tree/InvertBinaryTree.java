b993c39f3b4b795648c5528664c2099b686f9649
Uybzil <BigUncleLi@163.com>
2020-02-27 10:59:07 +0800

    feat : add invert binary tree implement

--- src/main/java/lizbyu/binary/tree/InvertBinaryTree.java
+++ src/main/java/lizbyu/binary/tree/InvertBinaryTree.java
@@ -13,6 +13,21 @@ public class InvertBinaryTree {
 	}
 
 	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+
+		TreeNode temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+
+		invertTree(root.left);
+		invertTree(root.right);
+
+		return root;
+	}
+
+	public TreeNode invertTree0(TreeNode root) {
 		if(root != null){
 			if (root.left == null && root.right == null) {
 				return root;
