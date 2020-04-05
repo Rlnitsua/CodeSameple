e6ce084ac9ebaf05f2857f94a0ad2149ae0821bf
Uybzil <BigUncleLi@163.com>
2020-04-05 11:58:29 +0800

    feat : add num of reduce number solution

---
+++ src/main/java/lizbyu/contest/NumberofStepsReduceaNumberinX.java
@@ -0,0 +1,66 @@
+package lizbyu.contest;
+
+
+public class NumberofStepsReduceaNumberinX {
+
+    public int numSteps(String s) {
+        int time = 0;
+
+        while(s.length() != 1) {
+            char[] chArr = s.toCharArray();
+            if (chArr[chArr.length - 1] == '1') {
+                int index = -1;
+                for (int j = chArr.length - 1; j >= 0; j--) {
+                    if (chArr[j] != '1') {
+                        index = j;
+                        break;
+                    }
+                }
+
+                for (int j = index + 1; j < chArr.length; j++) {
+                    chArr[j] = '0';
+                }
+
+                if (index != -1) {
+                    chArr[index] = '1';
+                    s = new String(chArr);
+                } else {
+                    s = "1" + new String(chArr);
+                }
+                time++;
+            } else {
+                s = s.substring(0, s.length() - 1);
+                time++;
+            }
+        }
+
+        return time;
+    }
+
+    // time limited
+    /*public int numSteps(String s) {
+        int num = calculateNum(s);
+        int time = 0;
+
+        while(num != 1) {
+            if (num % 2 != 0) {
+                num++;
+                time++;
+            }
+            num /= 2;
+            time++;
+        }
+
+        return time;
+    }
+
+    private int calculateNum(String s) {
+        int num = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            num += Integer.parseInt(String.valueOf(s.charAt(i))) * Math.pow(2, s.length() - i - 1);
+        }
+
+        return num;
+    }*/
+}
---
+++ src/test/java/lizbyu/contest/NumberofStepsReduceaNumberinXTest.java
@@ -0,0 +1,36 @@
+package lizbyu.contest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class NumberofStepsReduceaNumberinXTest {
+    private String s;
+    private int res;
+
+    public NumberofStepsReduceaNumberinXTest(String s, int res) {
+        this.s = s;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> prepareData() {
+        return Arrays.asList(new Object[][]{
+                {"1101", 6},
+                {"10", 1},
+                {"1", 0},
+        });
+    }
+
+    @Test
+    public void numSteps() {
+        NumberofStepsReduceaNumberinX demo = new NumberofStepsReduceaNumberinX();
+        assertEquals(res, demo.numSteps(s));
+    }
+}
