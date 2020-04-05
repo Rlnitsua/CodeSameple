d22083c929dbf5c862963a893d1ac866924957a0
Uybzil <BigUncleLi@163.com>
2020-04-05 11:58:16 +0800

    feat : add longest happy string solution

---
+++ src/main/java/lizbyu/contest/LongestHappyString.java
@@ -0,0 +1,63 @@
+package lizbyu.contest;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestHappyString {
+    public String longestDiverseString(int a, int b, int c) {
+        Map<Character, Integer> info = initInfo(a, b, c);
+        return getString('0', info);
+    }
+
+    private String getString(char exclude, Map<Character, Integer> info) {
+        StringBuilder res = new StringBuilder();
+
+        for (Map.Entry<Character, Integer> entry : info.entrySet()) {
+            if (entry.getKey() == exclude) {
+                continue;
+            }
+            if (entry.getValue() >= 2) {
+                if (!isBigest(info.values(), entry.getValue())) {
+                    continue;
+                }
+                res.append(entry.getKey());
+                res.append(entry.getKey());
+                info.put(entry.getKey(), entry.getValue() - 2);
+                res.append(getString(entry.getKey(), info));
+                return new String(res);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : info.entrySet()) {
+            if (entry.getKey() == exclude) {
+                continue;
+            }
+            if (entry.getValue() >= 1) {
+                res.append(entry.getKey());
+                info.put(entry.getKey(), entry.getValue() - 1);
+                res.append(getString(entry.getKey(), info));
+                return new String(res);
+            }
+        }
+
+        return new String(res);
+    }
+
+    private boolean isBigest(Collection<Integer> entrySet, Integer val) {
+        for (Integer num : entrySet) {
+            if (num > val) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Map<Character, Integer> initInfo(int a, int b, int c) {
+        Map<Character, Integer> info = new HashMap<>();
+        info.put('a', a);
+        info.put('b', b);
+        info.put('c', c);
+        return info;
+    }
+}
---
+++ src/test/java/lizbyu/contest/LongestHappyStringTest.java
@@ -0,0 +1,41 @@
+package lizbyu.contest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class LongestHappyStringTest {
+    private int a;
+    private int b;
+    private int c;
+    private String res;
+
+    public LongestHappyStringTest(int a, int b, int c, String res) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> prepareData() {
+        return Arrays.asList(new Object[][]{
+                {1, 1, 7, "ccaccbcc"},
+                {2, 2, 1, "aabbc"},
+                {7, 1, 0, "aabaa"},
+                {4, 4, 3, "aabbccaabbc"}
+        });
+    }
+
+    @Test
+    public void longestDiverseString() {
+        LongestHappyString demo = new LongestHappyString();
+        assertEquals(demo.longestDiverseString(a, b, c), res);
+    }
+}
