32124c7e9e3d53c372ff82281f9cb1912b00da2a
Uybzil <lee.cleancoder@gmail.com>
2020-04-24 11:55:04 +0800

    feat : add Jewelsand implement

---
+++ src/main/java/lizbyu/hash/table/JewelsandStones.java
@@ -0,0 +1,36 @@
+package lizbyu.hash.table;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JewelsandStones {
+    public int numJewelsInStones(String J, String S) {
+        int nums = 0;
+
+        Map<Character, Integer> info = getInfo(S);
+
+        for (char ch : info.keySet()) {
+            for (char jCh : J.toCharArray()) {
+                if (ch == jCh) {
+                    nums += info.get(ch);
+                    break;
+                }
+            }
+        }
+        return nums;
+    }
+
+    private Map<Character, Integer> getInfo(String s) {
+        Map<Character, Integer> info = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            if (info.containsKey(ch)) {
+                info.put(ch, info.get(ch) + 1);
+            } else {
+                info.put(ch, 1);
+            }
+        }
+
+        return info;
+    }
+}
