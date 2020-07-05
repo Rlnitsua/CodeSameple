520e93afed03fa3523838d4204122da3fcd65b00
Uybzil <lee.cleancoder@gmail.com>
2020-07-05 15:04:25 +0800

    feat: add last moment ants solution

---
+++ src/test/java/lizbyu/contest/LastMomentBeforeAllAntsFallOutofaPlankTest.java
@@ -0,0 +1,43 @@
+package lizbyu.contest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class LastMomentBeforeAllAntsFallOutofaPlankTest {
+    private int n;
+    private int[] left;
+    private int[] right;
+    private int time;
+
+    public LastMomentBeforeAllAntsFallOutofaPlankTest(int n, int[] left, int[] right, int time) {
+        this.n = n;
+        this.left = left;
+        this.right = right;
+        this.time = time;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> prepareData() {
+        return Arrays.asList(new Object[][]{
+                {4, new int[]{4, 3}, new int[]{0, 1}, 4},
+                {7, new int[]{}, new int[]{0, 1, 2, 3, 4, 5, 6, 7}, 7},
+                {7, new int[]{0, 1, 2, 3, 4, 5, 6, 7}, new int[]{}, 7},
+                {9, new int[]{5}, new int[]{4}, 5},
+                {6, new int[]{6}, new int[]{0}, 6},
+        });
+    }
+
+    @Test
+    public void test() {
+        LastMomentBeforeAllAntsFallOutofaPlank demo = new LastMomentBeforeAllAntsFallOutofaPlank();
+        assertEquals(time, demo.getLastMoment(n, left, right));
+    }
+}
