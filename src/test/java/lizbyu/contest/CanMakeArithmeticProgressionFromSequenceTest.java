9565fc72994ada15639f67eb4583fdd79c0e5ce5
Uybzil <lee.cleancoder@gmail.com>
2020-07-05 10:53:50 +0800

    feat: add arithmetic progression solution

---
+++ src/test/java/lizbyu/contest/CanMakeArithmeticProgressionFromSequenceTest.java
@@ -0,0 +1,35 @@
+package lizbyu.contest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class CanMakeArithmeticProgressionFromSequenceTest {
+    private int[] arr;
+    private boolean res;
+
+    public CanMakeArithmeticProgressionFromSequenceTest(int[] arr, boolean res) {
+        this.arr = arr;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> prepareData() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{3, 5, 1}, true},
+                {new int[]{1, 2, 4}, false}
+        });
+    }
+
+    @Test
+    public void test() {
+        CanMakeArithmeticProgressionFromSequence demo = new CanMakeArithmeticProgressionFromSequence();
+        assertEquals(res, demo.canMakeArithmeticProgression(arr));
+    }
+}
