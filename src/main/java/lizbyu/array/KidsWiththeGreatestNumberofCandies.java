9275223109ed6d2c3cc6686ce060ac5235be37c0
Uybzil <lee.cleancoder@gmail.com>
2020-07-22 11:07:42 +0800

    feat: add kids with greatest num solution

---
+++ src/main/java/lizbyu/array/KidsWiththeGreatestNumberofCandies.java
@@ -0,0 +1,18 @@
+package lizbyu.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWiththeGreatestNumberofCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for (int candy : candies) {
+            max = Math.max(candy, max);
+        }
+        List<Boolean> res = new ArrayList<>();
+        for (int candy : candies) {
+            res.add(candy + extraCandies >= max);
+        }
+        return res;
+    }
+}
