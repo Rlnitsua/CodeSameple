6f337d6a6d910d92f94d0d08cf0040000a37d015
Uybzil <lee.cleancoder@gmail.com>
2020-07-22 12:00:46 +0800

    feat: add XOR operation solution

---
+++ src/test/java/lizbyu/bit/manipulation/XOROperationinanArrayTest.java
@@ -0,0 +1,39 @@
+package lizbyu.bit.manipulation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class XOROperationinanArrayTest {
+    private int n;
+    private int start;
+    private int res;
+
+    public XOROperationinanArrayTest(int n, int start, int res) {
+        this.n = n;
+        this.start = start;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {5,0,8},
+                {4,3,8},
+                {1,7,7},
+                {10,5,2},
+        });
+    }
+
+    @Test
+    public void test() {
+        XOROperationinanArray demo = new XOROperationinanArray();
+        assertEquals(res, demo.xorOperation(n, start));
+    }
+}
