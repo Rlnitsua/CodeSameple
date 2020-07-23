5f851cee7fd68ef0a64a274850a47648e141d382
Uybzil <lee.cleancoder@gmail.com>
2020-07-23 12:15:04 +0800

    feat: add max increase to keep city skyline solution

---
+++ src/test/java/lizbyu/array/MaxIncreasetoKeepCitySkylineTest.java
@@ -0,0 +1,34 @@
+package lizbyu.array;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class MaxIncreasetoKeepCitySkylineTest {
+    private int[][] grid;
+    private int res;
+
+    public MaxIncreasetoKeepCitySkylineTest(int[][] grid, int res) {
+        this.grid = grid;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[][]{{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}}, 35},
+        });
+    }
+
+    @Test
+    public void test() {
+        MaxIncreasetoKeepCitySkyline demo = new MaxIncreasetoKeepCitySkyline();
+        assertEquals(res, demo.maxIncreaseKeepingSkyline(grid));
+    }
+}
