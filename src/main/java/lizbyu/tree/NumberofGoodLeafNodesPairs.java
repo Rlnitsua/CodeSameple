b56f90dac1c9c9c8531dfeeacdd17738714f7c46
Uybzil <lee.cleancoder@gmail.com>
2020-07-24 15:08:53 +0800

    feat: add number of good leaf nodes paires

---
+++ src/main/java/lizbyu/tree/NumberofGoodLeafNodesPairs.java
@@ -0,0 +1,62 @@
+package lizbyu.tree;
+
+import lizbyu.utils.node.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberofGoodLeafNodesPairs {
+    public int countPairs(TreeNode root, int distance) {
+        preHandle(root);
+        List<String> list = new ArrayList<>();
+        calculateList(root, list, "");
+        int res = 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (compareString(list.get(i), list.get(j)) <= distance) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    int val = 0;
+    private void preHandle(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        root.val = ++val;
+        preHandle(root.left);
+        preHandle(root.right);
+    }
+
+    private void calculateList(TreeNode root, List<String> list, String s) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            list.add(s);
+        }
+        if (root.left != null) {
+            calculateList(root.left, list, s + root.val + root.left.val + "-");
+        }
+        if (root.right != null) {
+            calculateList(root.right, list, s + root.val + root.right.val + "-");
+        }
+    }
+
+    private int compareString(String p, String q) {
+        String[] pSplit = p.split("-");
+        String[] qSplit = q.split("-");
+        int time = 0;
+        for (int i = 0; i < Math.min(pSplit.length, qSplit.length); i++) {
+            if (pSplit[i].equals(qSplit[i])) {
+                time++;
+            } else {
+                break;
+            }
+        }
+        return pSplit.length + qSplit.length - (2 * time);
+    }
+}
---
+++ src/test/java/lizbyu/tree/NumberofGoodLeafNodesPairsTest.java
@@ -0,0 +1,43 @@
+package lizbyu.tree;
+
+import lizbyu.utils.node.TreeNode;
+import lizbyu.utils.node.TreeNodes;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class NumberofGoodLeafNodesPairsTest {
+    private TreeNode root;
+    private int distance;
+    private int res;
+
+    public NumberofGoodLeafNodesPairsTest(TreeNode root, int distance, int res) {
+        this.root = root;
+        this.distance = distance;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {TreeNodes.create(Arrays.asList(1,2,3,null,4)), 3,1},
+                {TreeNodes.create(Arrays.asList(1,2,3,4,5,6,7)), 3,2},
+                {TreeNodes.create(Arrays.asList(7,1,4,6,null,5,3,null,null,null,null,null,2)), 3,1},
+                {TreeNodes.create(Collections.singletonList(100)), 1,0},
+                {TreeNodes.create(Arrays.asList(1,1,1)), 2,1},
+        });
+    }
+
+    @Test
+    public void test() {
+        NumberofGoodLeafNodesPairs demo = new NumberofGoodLeafNodesPairs();
+        assertEquals(res, demo.countPairs(root, distance));
+    }
+}
