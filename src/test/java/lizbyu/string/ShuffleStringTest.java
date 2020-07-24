1e8a7030ad82c2feb25bd5574c3962b535d560e8
Uybzil <lee.cleancoder@gmail.com>
2020-07-24 15:08:41 +0800

    feat: add shuffle string

---
+++ src/test/java/lizbyu/string/ShuffleStringTest.java
@@ -0,0 +1,40 @@
+package lizbyu.string;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class ShuffleStringTest {
+    private String s;
+    private int[] indices;
+    private String res;
+
+    public ShuffleStringTest(String s, int[] indices, String res) {
+        this.s = s;
+        this.indices = indices;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"codeleet", new int[]{4,5,6,7,0,2,1,3}, "leetcode"},
+                {"abc", new int[]{0,1,2}, "abc"},
+                {"aiohn", new int[]{3,1,4,2,0}, "nihao"},
+                {"aaiougrt", new int[]{4,0,2,6,7,3,1,5}, "arigatou"},
+                {"art", new int[]{1,0,2}, "rat"},
+        });
+    }
+
+    @Test
+    public void test() {
+        ShuffleString demo = new ShuffleString();
+        assertEquals(res, demo.restoreString(s, indices));
+    }
+}
