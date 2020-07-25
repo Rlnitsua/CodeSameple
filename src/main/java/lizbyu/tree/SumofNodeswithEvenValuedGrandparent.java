cf831232257a73a666249574a6eb86f75ef34f5f
Uybzil <lee.cleancoder@gmail.com>
2020-07-25 14:19:13 +0800

    feat: add Sum of Node with Even val solution

---
+++ src/main/java/lizbyu/tree/SumofNodeswithEvenValuedGrandparent.java
@@ -0,0 +1,32 @@
+package lizbyu.tree;
+
+import lizbyu.utils.node.TreeNode;
+
+public class SumofNodeswithEvenValuedGrandparent {
+    public int sumEvenGrandparent(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int res = 0;
+        if (root.val % 2 == 0) {
+            if (root.left != null) {
+                if (root.left.left != null) {
+                    res += root.left.left.val;
+                }
+                if (root.left.right != null) {
+                    res += root.left.right.val;
+                }
+            }
+            if (root.right != null) {
+                if (root.right.left != null) {
+                    res += root.right.left.val;
+                }
+                if (root.right.right != null) {
+                    res += root.right.right.val;
+                }
+            }
+        }
+        return res + sumEvenGrandparent(root.left) + sumEvenGrandparent(root.right);
+    }
+}
---
+++ src/test/java/lizbyu/tree/SumofNodeswithEvenValuedGrandparentTest.java
@@ -0,0 +1,37 @@
+package lizbyu.tree;
+
+import lizbyu.utils.node.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SumofNodeswithEvenValuedGrandparentTest {
+
+    @Test
+    public void test() {
+        TreeNode treeNode = new TreeNode(6);
+        TreeNode treeNode1 = new TreeNode(7);
+        TreeNode treeNode2 = new TreeNode(8);
+        treeNode.left = treeNode1;
+        treeNode.right = treeNode2;
+        TreeNode treeNode3 = new TreeNode(2);
+        TreeNode treeNode4 = new TreeNode(7);
+        treeNode1.left = treeNode3;
+        treeNode1.right = treeNode4;
+        TreeNode treeNode5 = new TreeNode(1);
+        TreeNode treeNode6 = new TreeNode(3);
+        treeNode2.left = treeNode5;
+        treeNode2.right = treeNode6;
+        TreeNode treeNode7 = new TreeNode(9);
+        treeNode3.left = treeNode7;
+        TreeNode treeNode8 = new TreeNode(1);
+        TreeNode treeNode9 = new TreeNode(4);
+        treeNode4.left = treeNode8;
+        treeNode4.right = treeNode9;
+        TreeNode treeNode10 = new TreeNode(5);
+        treeNode6.right = treeNode10;
+
+        SumofNodeswithEvenValuedGrandparent demo = new SumofNodeswithEvenValuedGrandparent();
+        assertEquals(18, demo.sumEvenGrandparent(treeNode));
+    }
+}
