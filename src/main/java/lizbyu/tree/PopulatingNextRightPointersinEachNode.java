52b892ca873295c4b3c5de591c91f0a07e08eaa6
Uybzil <lee.cleancoder@gmail.com>
2020-08-05 17:50:00 +0800

    feat: add populat next right pointer solution

---
+++ src/main/java/lizbyu/tree/PopulatingNextRightPointersinEachNode.java
@@ -0,0 +1,57 @@
+package lizbyu.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PopulatingNextRightPointersinEachNode {
+    @SuppressWarnings("unused")
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(queue.size() > 0) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Node p = queue.poll();
+                assert p != null;
+
+                if (i < size - 1) {
+                    p.next = queue.peek();
+                }
+
+                if (p.left != null) {
+                    queue.add(p.left);
+                }
+                if (p.right != null) {
+                    queue.add(p.right);
+                }
+            }
+        }
+        return root;
+    }
+}
