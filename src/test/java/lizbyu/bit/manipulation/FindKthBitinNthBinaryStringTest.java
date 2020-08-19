7a2959f639499c94561424472ee6ec2aa3fc9472
Uybzil <lee.cleancoder@gmail.com>
2020-08-19 15:39:13 +0800

    feat: add find kth biti bs

---
+++ src/test/java/lizbyu/bit/manipulation/FindKthBitinNthBinaryStringTest.java
@@ -0,0 +1,39 @@
+package lizbyu.bit.manipulation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class FindKthBitinNthBinaryStringTest {
+    private int n;
+    private int k;
+    private char res;
+
+    public FindKthBitinNthBinaryStringTest(int n, int k, char res) {
+        this.n = n;
+        this.k = k;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {3, 1, '0'},
+                {4, 11, '1'},
+                {1, 1, '0'},
+                {2, 3, '1'},
+        });
+    }
+
+    @Test
+    public void test() {
+        FindKthBitinNthBinaryString demo = new FindKthBitinNthBinaryString();
+        assertEquals(res, demo.findKthBit(n, k));
+    }
+}
