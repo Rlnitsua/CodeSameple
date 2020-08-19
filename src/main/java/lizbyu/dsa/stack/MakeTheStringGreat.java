abe27f032d3713791d00f9208676532fbd11727d
Uybzil <lee.cleancoder@gmail.com>
2020-08-19 15:39:39 +0800

    feat: add make the string great solution

---
+++ src/main/java/lizbyu/dsa/stack/MakeTheStringGreat.java
@@ -0,0 +1,36 @@
+package lizbyu.dsa.stack;
+
+import java.util.Stack;
+
+public class MakeTheStringGreat {
+    public String makeGood(String s) {
+        if (s.length() == 1) {
+            return s;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (stack.isEmpty()) {
+                stack.push(s.charAt(i));
+                continue;
+            }
+
+            if (isNotGood(s.charAt(i), stack.peek())) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (stack.size() != 0) {
+            sb.append(stack.pop());
+        }
+
+        return sb.toString();
+    }
+
+    private boolean isNotGood(char ch1, char ch2) {
+        return Math.abs(ch1 - ch2) == 32;
+    }
+}
---
+++ src/test/java/lizbyu/dsa/stack/MakeTheStringGreatTest.java
@@ -0,0 +1,36 @@
+package lizbyu.dsa.stack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class MakeTheStringGreatTest {
+    private String s;
+    private String res;
+
+    public MakeTheStringGreatTest(String s, String res) {
+        this.s = s;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"leEeetcode", "leetcode"},
+                {"abBAcC", ""},
+                {"s", "s"},
+        });
+    }
+
+    @Test
+    public void test() {
+        MakeTheStringGreat demo = new MakeTheStringGreat();
+        assertEquals(res, demo.makeGood(s));
+    }
+}
