b91efb3ed2cc6f2ce23d7b3022d56821e8bb2a38
Uybzil <lee.cleancoder@gmail.com>
2020-08-19 15:38:55 +0800

    feat: add three consecutive sulution

---
+++ src/main/java/lizbyu/array/ThreeConsecutiveOdds.java
@@ -0,0 +1,18 @@
+package lizbyu.array;
+
+public class ThreeConsecutiveOdds {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int times = 0;
+        for (int value : arr) {
+            if (value % 2 != 0) {
+                times++;
+            } else {
+                times = 0;
+            }
+            if (times == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
