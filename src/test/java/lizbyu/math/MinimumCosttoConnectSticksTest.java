1e00589295edd74a70fcf54be91d20f77a97bd5f
Uybzil <lee.cleancoder@gmail.com>
2020-08-25 19:02:58 +0800

    feat: add min cost conn

---
+++ src/test/java/lizbyu/math/MinimumCosttoConnectSticksTest.java
@@ -0,0 +1,34 @@
+package lizbyu.math;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class MinimumCosttoConnectSticksTest {
+    private final int[] sticks;
+    private final int res;
+
+    public MinimumCosttoConnectSticksTest(int[] sticks, int res) {
+        this.sticks = sticks;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{3354,4316,3259,4904,4598,474,3166,6322,8080,9009}, 151646},
+        });
+    }
+
+    @Test
+    public void test() {
+        MinimumCosttoConnectSticks demo = new MinimumCosttoConnectSticks();
+        assertEquals(res, demo.connectSticks(sticks));
+    }
+}
