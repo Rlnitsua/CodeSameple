b10af5efc49858c3875b4824332eeb175522a5c6
Uybzil <lee.cleancoder@gmail.com>
2020-09-05 12:03:40 +0800

    feat: add pyramid

---
+++ src/test/java/lizbyu/dynamic/pyramidTest.java
@@ -0,0 +1,37 @@
+package lizbyu.dynamic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class pyramidTest {
+    private int n;
+    private List<Integer> k;
+    private int res;
+
+    public pyramidTest(int n, List<Integer> k, int res) {
+        this.n = n;
+        this.k = k;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {3, Arrays.asList(1, 2, 3), 9},
+        });
+    }
+
+    @Test
+    public void test() {
+        pyramid demo = new pyramid();
+        assertEquals(res, demo.pyramid(n, k));
+    }
+}
