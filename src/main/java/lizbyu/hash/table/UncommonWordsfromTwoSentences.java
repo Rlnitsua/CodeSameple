d9663d8421f136ae53e025c5520ab0a94d4f8770
Uybzil <lee.cleancoder@gmail.com>
2020-09-23 15:04:16 +0800

    feat: add uncommon words fromm two setences

---
+++ src/main/java/lizbyu/hash/table/UncommonWordsfromTwoSentences.java
@@ -0,0 +1,36 @@
+package lizbyu.hash.table;
+
+import java.util.*;
+
+public class UncommonWordsfromTwoSentences {
+    public String[] uncommonFromSentences(String A, String B) {
+        Map<String, Integer> aInfo = getSet(A);
+        Map<String, Integer> bInfo = getSet(B);
+
+        List<String> res = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : aInfo.entrySet()) {
+            if (entry.getValue() == 1 && !bInfo.containsKey(entry.getKey())) {
+                res.add(entry.getKey());
+            }
+        }
+        for (Map.Entry<String, Integer> entry : bInfo.entrySet()) {
+            if (entry.getValue() == 1 && !aInfo.containsKey(entry.getKey())) {
+                res.add(entry.getKey());
+            }
+        }
+
+        return res.toArray(new String[0]);
+    }
+
+    private Map<String, Integer> getSet(String a) {
+        Map<String, Integer> info = new HashMap<>();
+        for (String word : a.split(" ")) {
+            if (info.containsKey(word)) {
+                info.put(word, info.get(word) + 1);
+            } else {
+                info.put(word, 1);
+            }
+        }
+        return info;
+    }
+}
