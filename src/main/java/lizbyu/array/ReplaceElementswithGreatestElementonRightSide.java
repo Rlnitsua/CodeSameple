2608d3ebdfd1ee06f4b5e7030a733c395e9ed8a2
Uybzil <lee.cleancoder@gmail.com>
2020-09-27 16:38:30 +0800

    feat: add repalce elements with greatest element

---
+++ src/main/java/lizbyu/array/ReplaceElementswithGreatestElementonRightSide.java
@@ -0,0 +1,22 @@
+package lizbyu.array;
+
+import lizbyu.string.LengthofLastWord;
+
+public class ReplaceElementswithGreatestElementonRightSide {
+    public int[] replaceElements(int[] arr) {
+        int[] res = new int[arr.length];
+        int max = Integer.MIN_VALUE;
+        res[res.length - 1] = -1;
+
+        if (res.length == 1) {
+            return res;
+        }
+
+        for (int i = res.length - 2; i >= 0; i--) {
+            max = Math.max(max, arr[i + 1]);
+            res[i] = max;
+        }
+
+        return res;
+    }
+}
