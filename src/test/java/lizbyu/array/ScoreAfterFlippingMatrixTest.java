b9491d200a331297e546499057d36263b1e540f0
Uybzil <lee.cleancoder@gmail.com>
2020-10-12 21:37:05 +0800

    feat: add score after fliping matrix

---
+++ src/test/java/lizbyu/array/ScoreAfterFlippingMatrixTest.java
@@ -0,0 +1,34 @@
+package lizbyu.array;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class ScoreAfterFlippingMatrixTest {
+    private final int[][] A;
+    private final int res;
+
+    public ScoreAfterFlippingMatrixTest(int[][] a, int res) {
+        A = a;
+        this.res = res;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[][]{new int[]{0,0,1,1}, new int[]{1,0,1,0}, new int[]{1,1,0,0}}, 39}
+        });
+    }
+
+    @Test
+    public void test() {
+        ScoreAfterFlippingMatrix demo = new ScoreAfterFlippingMatrix();
+        assertEquals(res, demo.matrixScore(A));
+    }
+}
