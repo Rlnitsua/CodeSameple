973dea226c3be32fdcf2c697a24fb95141bd8ebb
Rlnitsua <lee.cleancoder@gmail.com>
2020-11-05 17:02:15 +0800

    feat: add reverse linked list

--- src/main/java/rlnitsua/list/ReverseLinkedList.java
+++ src/main/java/rlnitsua/list/ReverseLinkedList.java
@@ -1,62 +1,24 @@
 package rlnitsua.list;
 
-import rlnitsua.utils.log.LogUtils;
+import rlnitsua.utils.node.ListNode;
 
 public class ReverseLinkedList {
-    private static final String TAG = "ReverseLinkedList";
-
-    // Definition for singly-linked list.
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int x) {
-            val = x;
-        }
-    }
 
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+        if (head == null) {
+            return null;
         }
 
-        ListNode prev = head;
-        ListNode current = head.next;
-        prev.next = null;
-        ListNode next = null;
+        ListNode root = null;
+        ListNode last = null;
 
-        while (current != null) {
-            // init next value
-            next = current.next;
-            // do reverse
-            current.next = prev;
-            // move to next position
-            prev = current;
-            current = next;
-        }
-
-        return prev;
-    }
-
-    private void showLinkedList(ListNode head) {
         while (head != null) {
-            LogUtils.d(TAG, head.val);
+            root = new ListNode(head.val);
+            root.next = last;
+            last = root;
             head = head.next;
         }
-    }
 
-    public static void main(String[] args) {
-        ListNode head = createLinkedList();
-        ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
-        ListNode reverseHead = reverseLinkedList.reverseList(head);
-        reverseLinkedList.showLinkedList(reverseHead);
-    }
-
-    private static ListNode createLinkedList() {
-        ListNode node1 = new ReverseLinkedList().new ListNode(1);
-        ListNode node2 = new ReverseLinkedList().new ListNode(2);
-        node1.next = node2;
-        node2.next = null;
-        return node1;
+        return root;
     }
 }
