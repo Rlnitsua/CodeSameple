65daf2e9a5a569a90dcfd318aaa1750c68c1806e
Rlnitsua <lee.cleancoder@gmail.com>
2020-11-10 23:03:57 +0800

    feat: add traversal array

--- src/main/java/rlnitsua/array/TraversalArray.java
+++ src/main/java/rlnitsua/array/TraversalArray.java
@@ -1,66 +1,51 @@
 package rlnitsua.array;
 
 public class TraversalArray {
+    private int index = 0;
+
     public int[] printMatrix (int[][] matrix) {
-        Direction direction = Direction.RIGHT;
-
-        boolean[][] usedMatrix = new boolean[matrix.length][matrix[0].length];
-        int[] res = new int[matrix.length * matrix[0].length];
-        int index = 0;
-        int row = 0;
-        int column = 0;
-
-        while (index < res.length - 1) {
-            switch (direction) {
-                case RIGHT:
-                    if (column != matrix[0].length - 1 && !usedMatrix[row][column + 1]) {
-                        res[index] = matrix[row][column];
-                        usedMatrix[row][column] = true;
-                        index++;
-                        column++;
-                    } else {
-                        direction = Direction.DOWN;
-                    }
-                    break;
-                case DOWN:
-                    if (row != matrix.length - 1 && !usedMatrix[row + 1][column]) {
-                        res[index] = matrix[row][column];
-                        usedMatrix[row][column] = true;
-                        index++;
-                        row++;
-                    } else {
-                        direction = Direction.LEFT;
-                    }
-                    break;
-                case LEFT:
-                    if (column != 0 && !usedMatrix[row][column - 1]) {
-                        res[index] = matrix[row][column];
-                        usedMatrix[row][column] = true;
-                        index++;
-                        column--;
-                    } else {
-                        direction = Direction.UP;
-                    }
-                    break;
-                case UP:
-                    if (row != 0 && !usedMatrix[row - 1][column]) {
-                        res[index] = matrix[row][column];
-                        usedMatrix[row][column] = true;
-                        index++;
-                        row--;
-                    } else {
-                        direction = Direction.RIGHT;
-                    }
-                    break;
-            }
+        if (matrix.length == 0) {
+            return null;
         }
 
-        res[res.length - 1] = matrix[row][column];
+        int[] res = new int[matrix.length * matrix[0].length];
+        int beginRow = 0;
+        int beginColumn = 0;
+        int endRow = matrix.length - 1;
+        int endColumn = matrix[0].length - 1;
+
+        while (beginRow <= endRow && beginColumn <= endColumn) {
+            traversal(matrix, beginRow++, beginColumn++, endRow--, endColumn--, res);
+        }
+
         return res;
     }
 
+    private void traversal(int[][] matrix, int beginRow, int beginColumn, int endRow, int endColumn, int[] res) {
+        if (beginRow == endRow) {
+            for (int i = beginColumn; i <= endColumn; i++) {
+                res[index++] = matrix[beginRow][i];
+            }
+        } else if (beginColumn == endColumn) {
+            for (int i = beginRow; i <= endRow; i++) {
+                res[index++] = matrix[i][beginColumn];
+            }
+        } else {
+            int currentRow = beginRow;
+            int currentColumn = beginColumn;
 
-    private enum Direction {
-        RIGHT, DOWN, LEFT, UP
+            while (currentColumn < endColumn) {
+                res[index++] = matrix[currentRow][currentColumn++];
+            }
+            while (currentRow < endRow) {
+                res[index++] = matrix[currentRow++][currentColumn];
+            }
+            while (currentColumn > beginColumn) {
+                res[index++] = matrix[currentRow][currentColumn--];
+            }
+            while (currentRow > beginRow) {
+                res[index++] = matrix[currentRow--][currentColumn];
+            }
+        }
     }
 }
